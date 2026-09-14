0ff2ff6bc01bf4cd3a8bc26cdae8c0fdce5f3126
«duKe» <dyuk0vser@yandex.ru>
2026-09-14 20:20:33 +0300

    Start

---
+++ src/main/java/com/university/LearningManagementSystem/LearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.university.LearningManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LearningManagementSystemApplication.class, args);
+	}
+
+}
